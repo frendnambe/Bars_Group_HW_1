9738885a0d660d930335c279a64152bd2b8d2569
frendnambe <gabd.den@yandex.ru>
2022-03-23 11:35:13 +0300

    Добавьте файлы проекта.

---
+++ Bars_Group_HW_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bars_Group_HW1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var key = new Key();
+            key.OnKeyPressed += (Sender, ButtonKey) => Console.WriteLine(ButtonKey);
+
+            key.Run();
+        }
+
+        internal class Key
+        {
+            public event EventHandler<char>? OnKeyPressed;
+            public void Run()
+            {
+                char checkSymbol = Console.ReadKey().KeyChar;
+                while ((char.ToUpper(checkSymbol) != 'C'))
+                {
+                    OnKeyPressed?.Invoke(this, checkSymbol);
+                    checkSymbol = Console.ReadKey().KeyChar;
+                }
+            }
+        }
+    }
+}
